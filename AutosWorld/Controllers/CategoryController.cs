a8d0492b4900a2250f79a3251fe29877f4a7120f
samspecial <psalmueloye@gmail.com>
2022-06-30 14:50:25 +0100

    Add project files.

---
+++ AutosWorld/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using AutosWorld.Data;
+using AutosWorld.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AutosWorld.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public CategoryController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Category> categoryList = _dbContext.Categories.ToList();
+            return View(categoryList);
+        }
+    }
+}
---
+++ AutosWorld/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using AutosWorld.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AutosWorld.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
