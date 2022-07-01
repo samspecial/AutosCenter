4a98389664da3a8a7f5fc458df740e944af85aae
samspecial <psalmueloye@gmail.com>
2022-07-01 03:18:07 +0100

    Basic CRUD operations

--- AutosWorld/Controllers/CategoryController.cs
+++ AutosWorld/Controllers/CategoryController.cs
@@ -16,5 +16,71 @@ namespace AutosWorld.Controllers
             IEnumerable<Category> categoryList = _dbContext.Categories.ToList();
             return View(categoryList);
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category model)
+        {
+            if (model.Name == model.DisplayOrder.ToString())
+                ModelState.AddModelError("Custom Error", "Display name cannot match Name");
+            if (!ModelState.IsValid)
+                return View(model);
+            _dbContext.Categories.Add(model);
+            _dbContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+                return NotFound();
+            var category = _dbContext.Categories.Find(id);
+            var categoryFromDb = _dbContext.Categories.FirstOrDefault(c => c.Id == id);
+            if (categoryFromDb == null || category == null)
+                return NotFound();
+            return View(category);
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category model)
+        {
+            if (model.Name == model.DisplayOrder.ToString())
+                ModelState.AddModelError("Custom error", "Display name mismatch");
+            if (!ModelState.IsValid)
+                return View(model);
+            
+            _dbContext.Update(model);
+            _dbContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+                return NotFound();
+            var category = _dbContext.Categories.Find(id);
+            if (category == null)
+                return NotFound();
+            return View(category);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+            var obj = _dbContext.Categories.Find(id);
+            if (obj == null)
+                return NotFound();
+            _dbContext.Categories.Remove(obj);
+            _dbContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
--- AutosWorld/Models/Category.cs
+++ AutosWorld/Models/Category.cs
@@ -8,6 +8,8 @@ namespace AutosWorld.Models
         public int Id { get; set; }
         [Required]
         public string Name { get; set; }
+        [Display(Name ="display order")]
+        [Range(1,200, ErrorMessage ="Number cannot be greater than 200")]
         public int DisplayOrder { get; set; }
         public DateTime CreatedDateTime { get; set; } = DateTime.Now;
     }
