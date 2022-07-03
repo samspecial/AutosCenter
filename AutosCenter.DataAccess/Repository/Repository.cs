6215df9897beb36c4a7e14764ad86f35fdb14b34
samspecial <psalmueloye@gmail.com>
2022-07-03 03:19:57 +0100

    Created generics repository

---
+++ AutosCenter.DataAccess/Repository/Repository.cs
@@ -0,0 +1,52 @@
+﻿using AutosCenter.DataAccess.Repository.IRepository;
+using AutosCenterd.DataAccess;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutosCenter.DataAccess.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _dbContext;
+        internal DbSet<T> dbSet;
+
+        public Repository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+            this.dbSet = _dbContext.Set<T>();
+        }
+        public void Add(T entity)
+        {
+            dbSet.Add(entity);
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            IQueryable<T> query = dbSet;
+            return query.ToList();
+        }
+
+        public T GetFirstOrDefault(Expression<Func<T, bool>> filter)
+        {
+            IQueryable<T> query = dbSet;
+            query = query.Where(filter);
+            return query.FirstOrDefault(filter);
+
+        }
+
+        public void Remove(T entity)
+        {
+            dbSet.Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<T> entities)
+        {
+            dbSet.RemoveRange(entities);
+        }
+    }
+}
